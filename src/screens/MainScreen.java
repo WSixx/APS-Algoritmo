c3396eee0933743dbd882cefd575c6d9cbada454
Lucas <WSixx@users.noreply.github.com>
2020-09-14 21:10:16 -0300

    Fixed

---
+++ src/screens/MainScreen.java
@@ -0,0 +1,13 @@
+package screens;
+
+import javax.swing.*;
+
+public class MainScreen {
+    private JPanel panel1;
+    private JButton btnStart;
+    private JButton btnList;
+    private JScrollPane scrollPane;
+    private JList listData;
+    private JButton button1;
+    private JTextField indiceTextField;
+}
